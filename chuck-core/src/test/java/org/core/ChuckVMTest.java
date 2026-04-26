2249e1699d2e79f1b4ee1d3121ca9156bebb2a99
ludoch <ludo@google.com>
2026-04-26 11:19:37 -0700

    Fix timing test flaky failure in ChuckVMTest TAG=agy CONV=1d40e2d8-0d8a-4c68-8d92-77417d4199e5

--- chuck-core/src/test/java/org/core/ChuckVMTest.java
+++ chuck-core/src/test/java/org/core/ChuckVMTest.java
@@ -44,10 +44,11 @@ public class ChuckVMTest {
     assertEquals(25, vm.getCurrentTime());
     // Give virtual thread time to finish and set isDone
     int retries = 0;
-    while (!shred.isDone() && retries < 100) {
+    while (!shred.isDone() && retries < 500) {
       Thread.sleep(5);
       retries++;
     }
+
     assertTrue(shred.isDone(), "Shred did not finish in time");
   }
 
